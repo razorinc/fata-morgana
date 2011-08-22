2a958be7c3d37ab403b0d520ae3aecf544258077
Mark Lamourine <markllama@redhat.com>
2011-08-22 16:43:02 -0400

    added version unit test

---
+++ test/unit/version_test.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/ruby
+#
+# Test the Openshift SDK VERSION string
+#
+require 'test_helper'
+
+class TestVersion < Test::Unit::TestCase
+
+  def test_leading_space
+    assert_no_match(/^\s+/, 
+                    Openshift::SDK::VERSION, 
+                    "Version string must not have leading white space"
+                    )
+  end
+
+  def test_trailing_space
+    assert_no_match(/\s+$/,
+                    Openshift::SDK::VERSION,
+                    "Version string must not have trailing white space"
+                    )
+  end
+
+  def test_version_pattern
+    assert_match(/^(\d+)\.(\d+)\.(\d+)$/, 
+                 Openshift::SDK::VERSION,
+                 "Version string must be of the form N.N.N"
+                 )
+  end
+end
+
