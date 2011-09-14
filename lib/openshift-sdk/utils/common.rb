0fdbff9563e7d06cedbf9ad5cc676cfcdfee6cd1
Rajat Chopra <rchopra@redhat.com>
2011-09-13 18:25:02 -0700

    created common.rb for common utility functions

---
+++ lib/openshift-sdk/utils/common.rb
@@ -0,0 +1,37 @@
+# Copyright 2010 Red Hat, Inc.
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+require 'rubygems'
+require 'openshift-sdk/model/user'
+require 'openshift-sdk/model/application'
+
+module Openshift::SDK::Utils
+  def self.get_application
+    gid = Process::GID.eid
+    app_guid = Openshift::SDK::Model::GidApplicationMap.find(gid).app_guid
+    unless app_guid
+      print "Unable to find application owned by this user.\n"
+      return -404
+    end
+    app = Openshift::SDK::Model::Application.find(app_guid,gid.to_s)
+  end
+end
